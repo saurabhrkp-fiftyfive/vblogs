db111da8f367557bf454874c2c9ed4d9c91cbd2c
Saurabh Patel <saurabh.patel@fiftyfivetech.io>
2025-02-19 17:22:36 +0530

    feat(app): update main.ts to use ConfigService for dynamic port configuration and integrate WinstonLoggerService for logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { WinstonLoggerService } from './infra/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = app.get(WinstonLoggerService);
+  app.useLogger(logger);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port') || 3000;
+  await app.listen(port);
+  logger.log(`Microservice is running on port ${port}`, `Bootstrap`);
 }
+
 bootstrap();
