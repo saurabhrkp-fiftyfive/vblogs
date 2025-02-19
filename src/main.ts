62b54be2fb8f7b49009bd2dda0c17c9a053635b1
Saurabh Patel <saurabh.patel@fiftyfivetech.io>
2025-02-19 18:18:51 +0530

    feat(app): integrate LoggingInterceptor for enhanced request logging in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { WinstonLoggerService } from './infra/logger/logger.service';
+import { LoggingInterceptor } from './infra/loggerInterceptor/loggerInterceptor.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(WinstonLoggerService);
-  app.useLogger(logger);
+  app.useGlobalInterceptors(new LoggingInterceptor(logger));
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port') || 3000;
   await app.listen(port);
