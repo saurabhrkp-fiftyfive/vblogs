0d0fe98d0c82683a5579e20681ea845f1d623ead
Saurabh Patel <saurabh.patel@fiftyfivetech.io>
2025-02-19 18:52:46 +0530

    fix(config): streamline S3 configuration structure in config.service.ts

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -27,13 +27,11 @@ export default () => ({
   },
   s3Config: {
     bucket: process.env.AWS_S3_BUCKET_NAME,
-    config: {
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      },
-      region: process.env.AWS_S3_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
+    region: process.env.AWS_S3_REGION,
   },
   resetTokenValidity:
     (process.env.RESET_TOKEN_VALIDITY_HOURS
